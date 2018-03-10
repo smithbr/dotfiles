3c8317e5ea43433dfef5d852d1440a1faa1c0074
Brandon Smith <smithbr@users.noreply.github.com>
2018-03-10 14:23:27 -0800

    add url encode/decode aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -8,3 +8,7 @@ alias ip_local="ipconfig getifaddr en0"
 # Remove junk files
 alias cleanup="find ~ -type f \( -name 'jmeter.log' -o -name 'results.xml' -o -name '.zcompdump*' \) -ls -delete"
 alias cleanupds="find . -type f -name '*.DS_Store' -ls -delete"
+
+# Web tools
+alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1])"'
+alias urldecode='python -c "import sys, urllib as ul; print ul.unquote_plus(sys.argv[1])"'
