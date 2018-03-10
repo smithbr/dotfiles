d51996b6ac5156d1f59a662ef53567e335bf5a7b
Brandon Smith <smithbr@users.noreply.github.com>
2018-03-10 01:32:10 -0800

    Clean up external settings

---
+++ shell/external.sh
@@ -0,0 +1,28 @@
+# macOS
+export PATH=/usr/local/bin:$PATH
+
+# JMeter
+export JMETER_HOME=/usr/local/opt/jmeter
+export PATH=$JMETER_HOME/bin/jmeter:$PATH
+
+# Maven
+export M2_HOME=/usr/local/opt/maven
+export M2=$M2_HOME/bin
+export MAVEN_OPTS="-Xmx1048m -Xms256m -XX:MaxPermSize=312M"
+export PATH=$M2:$PATH
+
+# Gatling
+export GATLING_HOME=/usr/local/opt/gatling
+export PATH=$GATLING_HOME/bin/gatling.sh:$PATH
+
+# InfluxDB
+export INFLUXDB_CONFIG_PATH=/usr/local/etc/influxdb.conf
+
+# python
+# virtualenvwrapper
+export WORKON_HOME=~/.virtualenvs
+source /usr/local/bin/virtualenvwrapper.sh
+# pip should only run if there is a virtualenv activated
+export PIP_REQUIRE_VIRTUALENV=true
+# Cache pip-installed packages to avoid re-downloading
+export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
