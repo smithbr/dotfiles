24dabd8969fb091ebce6ca7a679aa36016fd8d75
Brandon Smith <smithbr@users.noreply.github.com>
2018-03-10 01:48:27 -0800

    Clean up aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,15 +1,10 @@
-# navigation
+# Navigation
 alias lsa="ls -FGlAhp"
-alias f="open -a Finder ./"
 
-# cleanup
-alias cleanup="find ~ -type f \( -name 'jmeter.log' -o -name 'results.xml' -o -name '.zcompdump*' \) -ls -delete"
-alias cleanupds="find . -type f -name '*.DS_Store' -ls -delete"
-
-# network
+# Network
 alias ip_public="curl ipinfo.io/ip"
 alias ip_local="ipconfig getifaddr en0"
 
-# os x finder hidden files
-alias hf_show="defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app"
-alias hf_hide="defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app"
+# Remove junk files
+alias cleanup="find ~ -type f \( -name 'jmeter.log' -o -name 'results.xml' -o -name '.zcompdump*' \) -ls -delete"
+alias cleanupds="find . -type f -name '*.DS_Store' -ls -delete"
