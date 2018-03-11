d80135309c62b690ab94eb9868d9f1cacccd6a67
Brandon Smith <smithbr@users.noreply.github.com>
2018-03-10 17:59:52 -0800

    Add powerline fonts

---
+++ macOS/fonts.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo -e "\\n\\nInstalling fonts..."
+echo "========================================"
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts
+./install.sh
+cd ..
+rm -rf fonts
