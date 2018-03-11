af09b80cc5ab71166ca97343905e84de8dce9172
Brandon Smith <smithbr@users.noreply.github.com>
2018-03-11 00:27:08 -0800

    Add comments

--- linux/apt.sh
+++ linux/apt.sh
@@ -6,6 +6,15 @@ sudo apt-get update
 sudo apt-get upgrade
 
 sudo apt-get install -y build-essential
+
+echo -e "\\n\\nInstalling fonts...\\n"
+sudo apt-get install -y fonts-powerline
+
+echo -e "\\n\\nInstalling python and python-pip...\\n"
 sudo apt-get install -y python python-pip
-sudo apt-get install -y zsh
+
+echo -e "\\n\\nInstalling ruby...\\n"
 sudo apt-get install -y ruby-full
+
+echo -e "\\n\\nInstalling zsh...\\n"
+sudo apt-get install -y zsh
