6d843ecea380a5d0137108daa0553512d970f1e0
Brandon Smith <smithbr@users.noreply.github.com>
2018-03-10 23:10:20 -0800

    Move antigen

--- linux/brew.sh
+++ linux/brew.sh
@@ -63,6 +63,11 @@ for formula in "${formulas[@]}"; do
 done
 
 
+echo -e "\\n\\nInstalling antigen.zsh..."
+echo "========================================"
+curl -L git.io/antigen > /home/linuxbrew/.linuxbrew/bin/antigen.zsh
+
+
 echo -e "\\n\\nCleaning up..."
 echo "========================================"
 
