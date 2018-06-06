bd4781cb67ad739c1fd3c87da1425b2f4648c662
Brandon Smith <smithbr@users.noreply.github.com>
2018-06-06 16:43:47 -0700

    add awscli command

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -17,3 +17,6 @@ alias cleanupds="find . -type f -name '*.DS_Store' -ls -delete"
 # Web tools
 alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1])"'
 alias urldecode='python -c "import sys, urllib as ul; print ul.unquote_plus(sys.argv[1])"'
+
+# aws instances
+alias awsi="workon aws; aws ec2 describe-instances --query 'Reservations[].Instances[].[InstanceId, Tags[?Key==\"Name\"] | [0].Value, State.Name, PublicDnsName]' --output table"
