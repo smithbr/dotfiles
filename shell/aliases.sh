2137ac41e8e7b48ed240d7f472a37fc26f41abe6
Brandon Smith <1495361+smithbr@users.noreply.github.com>
2018-12-30 18:22:13 -0800

    Add portainer alias

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -31,3 +31,7 @@ alias awsi="workon aws; aws ec2 describe-instances --query 'Reservations[].Insta
 
 # start vnc server
 alias startvnc="sudo systemctl start x11vnc.service"
+
+# start portainer
+# portainer
+alias portainer="docker run -dit --restart unless-stopped -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer"
