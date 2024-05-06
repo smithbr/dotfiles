646cdc815716a0c7efb3fffc6d69df7a388d062d
Brandon Smith <smithbr@users.noreply.github.com>
2024-05-06 15:32:12 -0700

    Update aliases.sh

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -27,3 +27,6 @@ alias emptyfolders="du -a --max-depth=1 | sort -n"
 # encode/decode strings
 alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1])"'
 alias urldecode='python -c "import sys, urllib as ul; print ul.unquote_plus(sys.argv[1])"'
+
+# fix macOS time
+alias time="sudo sntp -sS time.apple.com"
