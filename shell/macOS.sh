6e8c2b8ea96e9ea10949888d85324b3580f06ed8
Brandon Smith <1495361+smithbr@users.noreply.github.com>
2026-02-13 03:07:52 -0800

    antigen updates

--- shell/macOS.sh
+++ shell/macOS.sh
@@ -4,3 +4,5 @@ alias f="open -a Finder ./"
 # Show/hide hidden files in Finder
 alias hf_show="defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app"
 alias hf_hide="defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app"
+
+export PATH="$HOME/.local/bin:$PATH"
