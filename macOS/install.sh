6e2b83f2afb79d3f723247f8d6be3625b9231240
Brandon Smith <1495361+smithbr@users.noreply.github.com>
2026-02-22 13:17:58 -0800

    Update install.sh

--- macOS/install.sh
+++ macOS/install.sh
@@ -3,8 +3,14 @@
 set -euo pipefail
 
 # via https://github.com/powerline/fonts/blob/master/README.rst
-git clone https://github.com/powerline/fonts.git --depth=1 $TMPDIR/fonts
-cd $TMPDIR/fonts
-./install.sh
-cd ..
-rm -rf $TMPDIR/fonts
+if compgen -G "$HOME/Library/Fonts/*Powerline*.ttf" >/dev/null;
+then
+    echo "Powerline fonts already installed... skipping."
+    exit 0
+fi
+
+tmp_fonts_dir="$(mktemp -d "${TMPDIR:-/tmp}/powerline-fonts.XXXXXX")"
+trap 'rm -rf "$tmp_fonts_dir"' EXIT
+
+git clone https://github.com/powerline/fonts.git --depth=1 "$tmp_fonts_dir"
+"$tmp_fonts_dir/install.sh"
