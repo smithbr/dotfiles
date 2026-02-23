0b86f3f6bc2cc7ecfab58b31e04de01cbae1879e
Brandon Smith <1495361+smithbr@users.noreply.github.com>
2026-02-22 19:30:38 -0800

    Add pnpm, curl

--- linux/install.sh
+++ linux/install.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 sudo -v
 sudo apt-get update
 sudo apt-get -y upgrade
-sudo apt-get install -y build-essential fonts-powerline zsh
+sudo apt-get install -y curl build-essential fonts-powerline zsh
