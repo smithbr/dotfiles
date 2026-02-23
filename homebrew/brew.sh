cf0e75889307c51e0d27a4a4efd2f468cb7f7a03
Brandon Smith <1495361+smithbr@users.noreply.github.com>
2026-02-22 22:51:48 -0800

    Update brew.sh

--- homebrew/brew.sh
+++ homebrew/brew.sh
@@ -7,24 +7,30 @@ if [[ "$(uname -s)" == "Darwin" ]]; then
     brewpath=homebrew
     if [[ "$(uname -m)" == "arm64" ]]; then
         brewbinpath=/opt/homebrew/bin
+        brewsbinpath=/opt/homebrew/sbin
     else
         brewbinpath=/usr/local/bin
+        brewsbinpath=/usr/local/sbin
     fi
 elif [[ "$(uname -s)" == "Linux" ]]; then
     brewplatform=Homebrew
     brewpath=homebrew
     brewbinpath=/home/linuxbrew/.linuxbrew/bin
+    brewsbinpath=/home/linuxbrew/.linuxbrew/sbin
 else
     echo "Unsupported OS: $(uname -s)" >&2
     exit 1
 fi
 
-export PATH="${brewbinpath}:${PATH}"
+export PATH="${brewbinpath}:${brewsbinpath}:${PATH}"
 
 if [[ ! -x "${brewbinpath}/brew" ]] && ! command -v brew >/dev/null 2>&1; then
     printf "\n\nInstalling %s...\n\n" "${brewpath}"
     /bin/bash -c "$(curl -fsSL "https://raw.githubusercontent.com/${brewplatform}/install/HEAD/install.sh")"
 fi
+
+brew_prefix="$(brew --prefix)"
+export PATH="${brew_prefix}/bin:${brew_prefix}/sbin:${PATH}"
 
 brew update && brew upgrade
 
