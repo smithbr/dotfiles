d204855da7de3dbe62648f0317a4d16f1a4c593f
Brandon Smith <1495361+smithbr@users.noreply.github.com>
2026-02-22 16:32:03 -0800

    Update install.sh

--- linux/install.sh
+++ linux/install.sh
@@ -3,10 +3,6 @@
 set -euo pipefail
 
 sudo -v
-
 sudo apt-get update
 sudo apt-get -y upgrade
-
-sudo apt-get install -y build-essential
-sudo apt-get install -y fonts-powerline
-sudo apt-get install -y zsh
+sudo apt-get install -y build-essential fonts-powerline zsh
