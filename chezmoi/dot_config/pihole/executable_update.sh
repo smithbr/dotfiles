36226a3a75d7b4c54c814f8eb4a998b634d80ed1
Brandon Smith <1495361+smithbr@users.noreply.github.com>
2026-03-01 14:10:49 -0800

    Update executable_update.sh

--- chezmoi/dot_config/pihole/executable_update.sh
+++ chezmoi/dot_config/pihole/executable_update.sh
@@ -5,18 +5,21 @@
 # sudo visudo -f /etc/sudoers.d/update-script
 # yourusername ALL=(ALL) NOPASSWD: /home/yourusername/update.sh
 
-# Update Pi-hole
-pihole -up
-
-# Update PADD
-~/padd.sh -u
-
 # Update packages
 apt-get update --fix-missing
 apt-get full-upgrade -y
 apt-get autoremove --purge -y
 apt-get autoclean -y
 apt-get clean
+
+# Update Unbound
+apt upgrade unbound
+
+# Update Pi-hole
+pihole -up
+
+# Update PADD
+~/padd.sh -u
 
 # Reboot the system
 # reboot
